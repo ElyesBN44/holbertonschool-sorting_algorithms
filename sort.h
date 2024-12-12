b35cb6f6604900f90081c6ff12733b2ac4887fab
root <root@Elyes.>
2024-12-12 11:55:06 +0100

    sorting

--- sort.h
+++ sort.h
@@ -1,7 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -19,5 +21,6 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void swap_ints(int *a, int *b);
 
 #endif
