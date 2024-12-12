6b9785e2751c9a281ffbd1ef389c360251d43864
root <root@Elyes.>
2024-12-12 11:57:31 +0100

    sorting

--- sort.h
+++ sort.h
@@ -4,6 +4,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -20,6 +35,9 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+
+
 void bubble_sort(int *array, size_t size);
 void swap_ints(int *a, int *b);
 
