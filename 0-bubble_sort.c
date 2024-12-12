cb16f8f978af382990d85df259d19c43284d21ed
root <root@Elyes.>
2024-12-12 11:27:18 +0100

    sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,14 +2,17 @@
 
 /**
  * bubble_sort - Sorts an array of integers.
- * @array: Pointer to the array to be sorted.
- * @size: The size of the array.
+ * using the bubble sort algorithm.
+ * @array: The array to be sorted
+ * @size: The number of elements in the array
  */
 void bubble_sort(int *array, size_t size)
 {
 size_t i, j;
 int temp;
 int swapped;
+if (!array || size < 2)
+return;
 for (i = 0; i < size - 1; i++)
 {
 swapped = 0;
@@ -24,7 +27,7 @@ print_array(array, size);
 swapped = 1;
 }
 }
-if (!swapped)
+if (swapped == 0)
 break;
 }
 }
